a4246e615a985ee33881935a68355c2f4ddb03dd
Isaac Sarte <aizakku09@gmail.com>
2022-02-21 15:55:04 +0800

    hand loading done

--- src/components/Hand/Hand.jsx
+++ src/components/Hand/Hand.jsx
@@ -8,21 +8,46 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHandPaper } from '@fortawesome/free-solid-svg-icons';
 
 // CSS
-import styles from './styles/hand.module.css';
+import '../../index.css';
+import styled from 'styled-components';
+
+const HandWave = styled(motion.div)`
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    padding-top: 20vh;
+`
+
+const Icon = styled.h1`
+    color: #B2856A; 
+    font-size: 5rem;
+
+    @media screen and (max-width: 500px) {
+        font-size: 3rem;
+    }
+`
+
+const Title = styled.h1`
+    margin-top: 1rem;
+    font-size: 3rem;
+
+    @media screen and (max-width: 500px) {
+        font-size: 2.5rem;
+    }
+`
 
 const Hand = () => {
     return (
-        <motion.div
-            className={styles.handShake}
-            initial={{ display: 'grid' }}
+        <HandWave
+            initial={{ display: 'flex' }}
             animate={{ display: 'none' }}
             transition={{ duration: 15, delay: 2 }}
         >
-            <div className={styles.welcomeBack}>
-                <h1 className={styles.hand}><FontAwesomeIcon icon={faHandPaper} /></h1>
-                <h1 className={styles.handText}>Welcome Back</h1>
+            <div>
+                <Icon className="hand"><FontAwesomeIcon icon={faHandPaper} /></Icon>
+                <Title>Welcome Back</Title>
             </div>
-        </motion.div>
+        </HandWave>
     )
 }
 
